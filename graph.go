c29eeb22d647296dde8d7d9c70613e3a2b6b93f6
Andreas Pape <andreas.pape@euroleasing.eu>
2021-02-26 14:57:48 +0100

    extract Graph from Pipe

--- graph.go
+++ graph.go
@@ -1,11 +1,59 @@
 package goflow
 
-type Graph interface {
-	Via(Flow) Graph
-	To(RunnableConsumer) Runnable
-}
-
 type Runnable interface {
 	Run() <-chan interface{}
 	Close()
 }
+
+type Graph interface {
+	Take(uint64) Graph
+	Map(interface{}) Graph
+	Filter(interface{}) Graph
+
+	Via(Flow) Graph
+	To(RunnableConsumer) Runnable
+}
+
+/* =================== */
+
+type ConsumerGraph interface {
+	Graph
+	WithProducer(Producer)
+}
+
+/* =================== */
+
+type FlowGraph interface {
+	ConsumerGraph
+	WithConsumer(Consumer)
+}
+
+/*
+type flowGraph struct {
+	sync.Mutex
+	inPipe  pipe
+	outPipe pipe
+}
+
+func (fg *flowGraph) WithProducer(Producer) {
+	fg.Lock()
+	defer fg.Unlock()
+
+}
+
+func (fg *flowGraph) WithConsumer(Consumer) {
+	fg.Lock()
+	defer fg.Unlock()
+
+}
+
+func (fg *flowGraph) Via(flow Flow) Graph {
+
+	return fg
+}
+
+func (fg *flowGraph) To(consumer RunnableConsumer) Runnable {
+
+	return nil
+}
+*/
